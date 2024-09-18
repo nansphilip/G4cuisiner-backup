4ed84260e00f70e0ebeb30587d9b4936d9ecd988
nansphilip <nansphilip@gmail.com>
2024-09-18 16:18:20 +0200

    Update db read me

--- prisma/sql/database.sql
+++ prisma/sql/database.sql
@@ -1,11 +1,11 @@
 -- Creates a database
-CREATE DATABASE `default-db`;
+CREATE DATABASE `g4cuisiner-db`;
 
 -- Selects the new database
-USE `default-db`;
+USE `g4cuisiner-db`;
 
 -- Creates an user and a password
-CREATE USER 'default-user'@'localhost' IDENTIFIED BY 'default-password';
+CREATE USER 'g4cuisiner-user'@'localhost' IDENTIFIED BY 'g4cuisiner-password';
 
 -- Allows user to connect to database
-GRANT ALL PRIVILEGES ON *.* TO 'default-user'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'g4cuisiner-user'@'localhost';
--- prisma/sql/reset.sql
+++ prisma/sql/reset.sql
@@ -2,16 +2,16 @@
 SELECT User, Host FROM mysql.user;
 
 -- Supprimer un utilisateur
-DROP USER 'default-user'@'localhost';
+DROP USER 'g4cuisiner-user'@'localhost';
 
 -- Afficher les privilèges d'un utilisateur
-SHOW GRANTS FOR 'default-user'@'localhost';
+SHOW GRANTS FOR 'g4cuisiner-user'@'localhost';
 
 -- Supprimer tous les privilèges d'un utilisateur
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'default-user'@'localhost';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'g4cuisiner-user'@'localhost';
 
 -- Assurer la mise à jour des privilèges après leur suppression
 FLUSH PRIVILEGES;
 
 -- Supprimer la base de donnée
-DROP DATABASE `default-db`;
+DROP DATABASE `g4cuisiner-db`;
